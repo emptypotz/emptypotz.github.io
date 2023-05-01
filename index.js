43e4c8e3be43f5de9e84e164c37eb520e9f9f78d
emptypotz <notablery@gmail.com>
2023-05-01 06:12:08 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function ScrollHome(){
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    })
+}
+
+function ScrollSkills(){
+    window.scrollTo({
+        top: 950,
+        left: 0,
+        behavior: "smooth",
+    })
+}
+
+function ScrollContact(){
+    window.scrollTo({
+        top: 2000,
+        left: 0,
+        behavior: "smooth",
+    })
+}
+
